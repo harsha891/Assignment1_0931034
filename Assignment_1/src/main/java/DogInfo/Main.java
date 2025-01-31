fd26ebb393151a29f72a8a8990e6d55367d4bc46
harsha891 <harsha8919485642@gmail.com>
2025-01-30 23:50:18 -0500

    Added comments

--- Assignment_1/src/main/java/DogInfo/Main.java
+++ Assignment_1/src/main/java/DogInfo/Main.java
@@ -2,9 +2,11 @@ package DogInfo;
 
 public class Main {
     public static void main(String arg[]) {
+        // Creating a person with no dog
         Person person = new Person("Harsha", 25);
 
         try {
+            // trying to changing the dog name of person
             person.changeDogsName("tom");
         } catch (Exception e) {
             System.out.println("Unable to change dogs name : " + e.getMessage());
